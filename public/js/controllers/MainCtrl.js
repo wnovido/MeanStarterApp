7eccc7ee26a9e2080a58daf94ff761094decc130
wilso_000 <wilson_novido@yahoo.com>
2014-11-18 11:14:41 -0500

    Completed the tutorial

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,7 +1,7 @@
 /**
  * Created by wilso_000 on 11/18/2014.
  */
-angular.module('MainCtrl', []).controller('MainController', function($score) {
+angular.module('MainCtrl', []).controller('MainController', function($scope) {
 	$scope.tagline = 'To the moon and back!';
 });
 
